63c8c56328e5c45c6eb7a09a1fd820b44f99d155
Simon Woolf <simon@simonwoolf.net>
2014-04-08 23:06:38 +0100

    cleanup, readme

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 recorder
 ========
+A pair of tiny scripts I wrote for my Dad to help him record stuff off the
+internet. They're written very specifically to his requirements and usecase, so may well not be useful to anyone else as-is. They both depend on zenity (and so gtk) for drawing dialogue boxes.
 
-Radio recorder &amp; iplayer downloader helper scripts
+recorder.sh is a bash script that records all sound output for a specified time. He uses it to record non-iplayer radio programs.
+
+iplayer.sh is a python script to download & convert iplayer radio
+programs given a url. Uses get\_iplayer.
--- iplayer.sh
+++ iplayer.sh
@@ -19,7 +19,4 @@ if 'episode' in url:
 elif 'programmes' in url:
     pid = url[url.index('programmes') + 1]
 
-
-#call("gnome-terminal --hide-menubar -x get_iplayer --modes=flashaachigh,flashaac,flashaacstd,flashaacstd1,flashaudio,flashhigh,flashstd,flashnormal --force --output ~/Music --pid " + pid + " --command 'ffmpeg -ab 192k -i <filename> `echo <filename> | sed s/aac/mp3/` && rm <filename>'", shell=True)
 call("gnome-terminal --hide-menubar -x get_iplayer --modes=flashaachigh,flashaac,flashaacstd,flashaacstd1,flashaudio,flashhigh,flashstd,flashnormal,flashaaclow --force --output ~/Music --pid " + pid, shell=True)
-#Removed conversion to mp3
